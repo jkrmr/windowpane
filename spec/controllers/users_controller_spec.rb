49fdc140cc38d643e3d9a4a06db4f6f9c8965a29
Jake Romer <jkrmr@jakeromer.com>
2017-08-09 18:28:14 -0400

    Add users controller, route, model, views

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #show" do
+    it "returns a success response"
+  end
+
+  describe "GET #new" do
+    it "returns a success response"
+  end
+end
