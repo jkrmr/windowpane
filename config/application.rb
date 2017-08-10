5eadbf83f196d37176f1211084d446e31fa42240
Jake Romer <jkrmr@jakeromer.com>
2017-08-10 10:37:44 -0400

    Disable some generators

--- config/application.rb
+++ config/application.rb
@@ -10,22 +10,36 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Windowpane
+
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified
+    # here.
+
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.factory_girl false
+      g.helper false
+      g.javascripts false
+      g.stylesheets false
+      g.test_framework :rspec, view_specs: false, routing_specs: false
+      g.template_engine nil
+    end
+
   end
+
 end
