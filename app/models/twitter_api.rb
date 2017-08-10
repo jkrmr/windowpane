a782172d32739bf6dee6d1bb7ae0f50cb05c9066
Jake Romer <jkrmr@jakeromer.com>
2017-08-09 23:17:54 -0400

    Add TwitterAPI facade

---
+++ app/models/twitter_api.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# A singleton facade for the twitter client
+class TwitterAPI
+
+  include Singleton
+
+  attr_reader :client
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = Figaro.env.twitter_consumer_key
+      config.consumer_secret = Figaro.env.twitter_consumer_secret
+      config.access_token = Figaro.env.twitter_access_token
+      config.access_token_secret = Figaro.env.twitter_access_secret
+    end
+  end
+
+  def fetch_tweets(username: "everythinggoats", count: 25)
+    client.user_timeline(username, count: count)
+  end
+
+end
