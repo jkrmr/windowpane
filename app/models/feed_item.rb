2a48b32ebfacd58ab44206892d6171f912a29638
Jake Romer <jkrmr@jakeromer.com>
2017-08-11 12:55:09 -0400

    Add at-mention parsing for tweet body

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# A decorator for a feed item from a twitter user's feed.
+# Initialized with a Hash
 class FeedItem < OpenStruct
 
   def self.from_list(list)
@@ -8,6 +10,35 @@ class FeedItem < OpenStruct
 
   def date_posted
     created_at.to_datetime.strftime("%b %-d %Y %l:%M%P")
+  end
+
+  def body
+    mentions = entities.fetch("user_mentions", [])
+    return text if mentions.empty?
+
+    replacements = mentions.each_with_object({}) do |mention, memo|
+      # ensure safe to mark safe
+      handle = h.sanitize(mention["screen_name"])
+
+      memo["@#{handle}"] =
+        h.link_to("@#{handle}", urls.feed_path(username: handle))
+    end
+
+    mentions_to_replace = Regexp.union(replacements.keys)
+
+    text.gsub(mentions_to_replace, replacements)
+        .html_safe # rubocop:disable Rails/OutputSafety
+        .freeze # freeze so html_safe string can't be modified elsewhere
+  end
+
+  private
+
+  def urls
+    Rails.application.routes.url_helpers
+  end
+
+  def h
+    ActionController::Base.helpers
   end
 
 end
