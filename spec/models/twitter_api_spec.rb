459d9cc38d7a37bfbf7c15a0f602722c7a3407e5
Jake Romer <jkrmr@jakeromer.com>
2017-08-10 22:07:18 -0400

    Add spec for TwitterAPI

---
+++ spec/models/twitter_api_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TwitterAPI, type: :model do
+  describe "#fetch_tweets" do
+    it "returns an iterable collection of tweets" do
+      VCR.use_cassette("twitter-api-everythinggoats-25") do
+        tweets = TwitterAPI
+                 .instance
+                 .fetch_tweets(username: "everythinggoats")
+
+        expect(tweets).to respond_to(:each)
+        expect(tweets.count).to eq(25)
+      end
+    end
+
+    it "accepts a target size for the queried collection" do
+      VCR.use_cassette("twitter-api-everythinggoats-5") do
+        tweets = TwitterAPI
+                 .instance
+                 .fetch_tweets(username: "everythinggoats", count: 5)
+
+        expect(tweets.count).to eq(5)
+        expect(tweets.first.user.username).to eq "EverythingGoats"
+      end
+    end
+
+    context "given a count of zero" do
+      it "returns the API default of 20 tweets" do
+        VCR.use_cassette("twitter-api-everythinggoats-0") do
+          tweets = TwitterAPI
+                   .instance
+                   .fetch_tweets(username: "everythinggoats", count: 0)
+          expect(tweets.count).to eq 20
+        end
+      end
+    end
+
+    context "given a twitter username associated with a private feed" do
+      it "returns an empty array" do
+        VCR.use_cassette("twitter-api-divya-25") do
+          tweets = TwitterAPI.instance.fetch_tweets(username: "divya")
+          expect(tweets).to eq []
+        end
+      end
+    end
+  end
+end
